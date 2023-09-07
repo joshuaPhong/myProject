ec9abb085f3ea61593e48ca5eb98f2b415b25d3c
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-09-08 04:54:48 +1200

    added app articles

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, ChangePasswordView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
-    
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('registration/password_change_done', ChangePasswordView.as_view(),
+         name='done')
+
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,5 @@ from .views import HomePageView, ChangePasswordView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('registration/password_change_done', ChangePasswordView.as_view(),
-         name='done')
 
 ]
