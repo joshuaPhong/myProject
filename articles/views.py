6fdf2a1a5cbbd5e778670b833104405075ef38ac
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-09-09 19:17:57 +1200

    added articles pview

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, FormView
+from .models import Article
 
-# Create your views here.
+
+# This view inherits from list view. Is like a select * from article. Get all
+# the data from a table
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
